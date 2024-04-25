19bdd2fd16f447e4b48d3f3a03b00d09a031829b
Manfred Bergmann <mb@software-by-mabe.com>
2024-04-25 19:35:52 +0200

    minor

--- src/knx-connect.lisp
+++ src/knx-connect.lisp
@@ -99,9 +99,8 @@ Returns `future:future` which resolves to `T' is all went well and error conditi
        (cond
          ((eq dpt-type 'dpt:dpt-1.001)
           (dpt:make-dpt1 dpt-type (if value :on :off)))
-         ((eq dpt-type 'dpt:dpt-5.001)
-          (dpt:make-dpt5 dpt-type value))
-         ((eq dpt-type 'dpt:dpt-5.010)
+         ((or (eq dpt-type 'dpt:dpt-5.001)
+              (eq dpt-type 'dpt:dpt-5.010))
           (dpt:make-dpt5 dpt-type value))
          ((eq dpt-type 'dpt:dpt-9.001)
           (dpt:make-dpt9 dpt-type value))
